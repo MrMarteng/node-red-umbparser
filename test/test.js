9083edc6c58ece2c042d4a6a8865203a2ed4d05d
Martin Kiepfer <m.kiepfer@teleschirm.org>
2020-02-23 21:27:57 +0100

    First status-frame test added

--- test/test.js
+++ test/test.js
@@ -3,11 +3,16 @@ const mod_umbparser = require('../umbparser')
 let assert = require('assert');
 let umbparser = new mod_umbparser.UMBParser();
 
-let statusFrame = new ByteArray() [ 0, 01, 05 ];
+let statusOkResponse = [ 0x01, 0x10, 0x01, 0xFF, 0x01, 0x50, 0x03, 0x02, 0x26, 0x10, 0x00, 0x03, 0xFF, 0xFF, 0x04 ];
+let status55Response = [ 0x01, 0x10, 0x01, 0xFF, 0x01, 0x50, 0x03, 0x02, 0x26, 0x10, 0x55, 0x03, 0xFF, 0xFF, 0x04 ];
 
 describe('Class', function() {
     describe('#status frame', function(){
-        umbparser.readBuffer(statusFrame);
+        it('UMB status requires with error status OK has been parsed correctly', function() {
+            umbstatus = umbparser.ParseReadBuf(statusOkResponse);
+            assert.equal(umbstatus.umbframe.status, 0);
+            assert.equal(umbstatus.parserState, "finished");
+        })
     })
 })
 
