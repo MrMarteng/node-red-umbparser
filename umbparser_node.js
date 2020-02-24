eef3561c2bcf70b97ea3bda5e994245bade66c4a
Martin Kiepfer <m.kiepfer@teleschirm.org>
2020-02-24 22:10:12 +0100

    parser node linked with latest UMBParser version

--- umbparser_node.js
+++ umbparser_node.js
@@ -7,10 +7,19 @@ module.exports = function(RED) {
         RED.nodes.createNode(this, config);
         var node = this;
         node.on('input', function(msg) {
-
             if( msg.payload instanceof Buffer )
             {
-                this.log("Heyho");
+                this.log("Valid input buffer detected");
+                let parsedFrame = umbparser.ParseReadBuf(msg.payload);
+
+                this.log("Parsing status:")
+                this.log("parser status: " + parsedFrame.parserState);
+                if(parsedFrame.parserState == "finished")
+                {
+                    this.log("Frametype: " + parsedFrame.umbframe.type);
+                    this.log("Framestatus: " + parsedFrame.umbframe.status);
+                }
+               
             }
             else
             {
